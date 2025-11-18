15b6aa4bcabffe51f6ca64579889751a18be1c92
camilorincon9850@gmail.com <camilorincon9850@gmail.com>
2025-11-18 00:53:38 -0500

    Refactor routes with Route::prefix() groups for cleaner organization

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
- //use Carbon\Carbon; 
-/*
 
+/*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
@@ -11,115 +10,117 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
- */
+*/
 
+Route::get('/', 'ControladorWebHome@index');
 
-/*Route::get('/time' , function(){$date =new Carbon;echo $date ; } );*/
+Route::prefix('admin')->group(function () {
+    
+    Route::get('', 'ControladorHome@index');
+    Route::post('patente/nuevo', 'ControladorPatente@guardar');
 
+    /* --------------------------------------------- */
+    /* CONTROLADOR LOGIN                           */
+    /* --------------------------------------------- */
+    Route::get('login', 'ControladorLogin@index');
+    Route::get('logout', 'ControladorLogin@logout');
+    Route::post('logout', 'ControladorLogin@entrar');
+    Route::post('login', 'ControladorLogin@entrar');
 
-Route::group(array('domain' => '127.0.0.1'), function () {
+    /* --------------------------------------------- */
+    /* CONTROLADOR RECUPERO CLAVE                    */
+    /* --------------------------------------------- */
+    Route::get('recupero-clave', 'ControladorRecuperoClave@index');
+    Route::post('recupero-clave', 'ControladorRecuperoClave@recuperar');
 
-    Route::get('/', 'ControladorWebHome@index');
- 
+    /* --------------------------------------------- */
+    /* CONTROLADOR PERMISO                           */
+    /* --------------------------------------------- */
+    Route::get('usuarios/cargarGrillaFamiliaDisponibles', 'ControladorPermiso@cargarGrillaFamiliaDisponibles')->name('usuarios.cargarGrillaFamiliaDisponibles');
+    Route::get('usuarios/cargarGrillaFamiliasDelUsuario', 'ControladorPermiso@cargarGrillaFamiliasDelUsuario')->name('usuarios.cargarGrillaFamiliasDelUsuario');
+    Route::get('permisos', 'ControladorPermiso@index');
+    Route::get('permisos/cargarGrilla', 'ControladorPermiso@cargarGrilla')->name('permiso.cargarGrilla');
+    Route::get('permiso/nuevo', 'ControladorPermiso@nuevo');
+    Route::get('permiso/cargarGrillaPatentesPorFamilia', 'ControladorPermiso@cargarGrillaPatentesPorFamilia')->name('permiso.cargarGrillaPatentesPorFamilia');
+    Route::get('permiso/cargarGrillaPatentesDisponibles', 'ControladorPermiso@cargarGrillaPatentesDisponibles')->name('permiso.cargarGrillaPatentesDisponibles');
+    Route::get('permiso/{idpermiso}', 'ControladorPermiso@editar');
+    Route::post('permiso/{idpermiso}', 'ControladorPermiso@guardar');
 
-    Route::get('/admin', 'ControladorHome@index');
-    Route::post('/admin/patente/nuevo', 'ControladorPatente@guardar');
+    /* --------------------------------------------- */
+    /* CONTROLADOR GRUPO                             */
+    /* --------------------------------------------- */
+    Route::get('grupos', 'ControladorGrupo@index');
+    Route::get('usuarios/cargarGrillaGruposDelUsuario', 'ControladorGrupo@cargarGrillaGruposDelUsuario')->name('usuarios.cargarGrillaGruposDelUsuario');
+    Route::get('usuarios/cargarGrillaGruposDisponibles', 'ControladorGrupo@cargarGrillaGruposDisponibles')->name('usuarios.cargarGrillaGruposDisponibles');
+    Route::get('grupos/cargarGrilla', 'ControladorGrupo@cargarGrilla')->name('grupo.cargarGrilla');
+    Route::get('grupo/nuevo', 'ControladorGrupo@nuevo');
+    Route::get('grupo/setearGrupo', 'ControladorGrupo@setearGrupo');
+    Route::post('grupo/nuevo', 'ControladorGrupo@guardar');
+    Route::get('grupo/{idgrupo}', 'ControladorGrupo@editar');
+    Route::post('grupo/{idgrupo}', 'ControladorGrupo@guardar');
 
-/* --------------------------------------------- */
-/* CONTROLADOR LOGIN                           */
-/* --------------------------------------------- */
-    Route::get('/admin/login', 'ControladorLogin@index');
-    Route::get('/admin/logout', 'ControladorLogin@logout');
-    Route::post('/admin/logout', 'ControladorLogin@entrar');
-    Route::post('/admin/login', 'ControladorLogin@entrar');
+    /* --------------------------------------------- */
+    /* CONTROLADOR USUARIO                           */
+    /* --------------------------------------------- */
+    Route::get('usuarios', 'ControladorUsuario@index');
+    Route::get('usuarios/nuevo', 'ControladorUsuario@nuevo');
+    Route::post('usuarios/nuevo', 'ControladorUsuario@guardar');
+    Route::post('usuarios/{usuario}', 'ControladorUsuario@guardar');
+    Route::get('usuarios/cargarGrilla', 'ControladorUsuario@cargarGrilla')->name('usuarios.cargarGrilla');
+    Route::get('usuarios/buscarUsuario', 'ControladorUsuario@buscarUsuario');
+    Route::get('usuarios/{usuario}', 'ControladorUsuario@editar');
 
-/* --------------------------------------------- */
-/* CONTROLADOR RECUPERO CLAVE                    */
-/* --------------------------------------------- */
-    Route::get('/admin/recupero-clave', 'ControladorRecuperoClave@index');
-    Route::post('/admin/recupero-clave', 'ControladorRecuperoClave@recuperar');
+    /* --------------------------------------------- */
+    /* CONTROLADOR MENU                             */
+    /* --------------------------------------------- */
+    Route::get('sistema/menu', 'ControladorMenu@index');
+    Route::get('sistema/menu/nuevo', 'ControladorMenu@nuevo');
+    Route::post('sistema/menu/nuevo', 'ControladorMenu@guardar');
+    Route::get('sistema/menu/cargarGrilla', 'ControladorMenu@cargarGrilla')->name('menu.cargarGrilla');
+    Route::get('sistema/menu/eliminar', 'ControladorMenu@eliminar');
+    Route::get('sistema/menu/{id}', 'ControladorMenu@editar');
+    Route::post('sistema/menu/{id}', 'ControladorMenu@guardar');
 
-/* --------------------------------------------- */
-/* CONTROLADOR PERMISO                           */
-/* --------------------------------------------- */
-    Route::get('/admin/usuarios/cargarGrillaFamiliaDisponibles', 'ControladorPermiso@cargarGrillaFamiliaDisponibles')->name('usuarios.cargarGrillaFamiliaDisponibles');
-    Route::get('/admin/usuarios/cargarGrillaFamiliasDelUsuario', 'ControladorPermiso@cargarGrillaFamiliasDelUsuario')->name('usuarios.cargarGrillaFamiliasDelUsuario');
-    Route::get('/admin/permisos', 'ControladorPermiso@index');
-    Route::get('/admin/permisos/cargarGrilla', 'ControladorPermiso@cargarGrilla')->name('permiso.cargarGrilla');
-    Route::get('/admin/permiso/nuevo', 'ControladorPermiso@nuevo');
-    Route::get('/admin/permiso/cargarGrillaPatentesPorFamilia', 'ControladorPermiso@cargarGrillaPatentesPorFamilia')->name('permiso.cargarGrillaPatentesPorFamilia');
-    Route::get('/admin/permiso/cargarGrillaPatentesDisponibles', 'ControladorPermiso@cargarGrillaPatentesDisponibles')->name('permiso.cargarGrillaPatentesDisponibles');
-    Route::get('/admin/permiso/{idpermiso}', 'ControladorPermiso@editar');
-    Route::post('/admin/permiso/{idpermiso}', 'ControladorPermiso@guardar');
+    /* --------------------------------------------- */
+    /* CONTROLADOR PATENTES                          */
+    /* --------------------------------------------- */
+    Route::get('patentes', 'ControladorPatente@index');
+    Route::get('patente/nuevo', 'ControladorPatente@nuevo');
+    Route::post('patente/nuevo', 'ControladorPatente@guardar');
+    Route::get('patente/cargarGrilla', 'ControladorPatente@cargarGrilla')->name('patente.cargarGrilla');
+    Route::get('patente/eliminar', 'ControladorPatente@eliminar');
+    Route::get('patente/nuevo/{id}', 'ControladorPatente@editar');
+    Route::post('patente/nuevo/{id}', 'ControladorPatente@guardar');
 
-/* --------------------------------------------- */
-/* CONTROLADOR GRUPO                             */
-/* --------------------------------------------- */
-    Route::get('/admin/grupos', 'ControladorGrupo@index');
-    Route::get('/admin/usuarios/cargarGrillaGruposDelUsuario', 'ControladorGrupo@cargarGrillaGruposDelUsuario')->name('usuarios.cargarGrillaGruposDelUsuario'); //otra cosa
-    Route::get('/admin/usuarios/cargarGrillaGruposDisponibles', 'ControladorGrupo@cargarGrillaGruposDisponibles')->name('usuarios.cargarGrillaGruposDisponibles'); //otra cosa
-    Route::get('/admin/grupos/cargarGrilla', 'ControladorGrupo@cargarGrilla')->name('grupo.cargarGrilla');
-    Route::get('/admin/grupo/nuevo', 'ControladorGrupo@nuevo');
-    Route::get('/admin/grupo/setearGrupo', 'ControladorGrupo@setearGrupo');
-    Route::post('/admin/grupo/nuevo', 'ControladorGrupo@guardar');
-    Route::get('/admin/grupo/{idgrupo}', 'ControladorGrupo@editar');
-    Route::post('/admin/grupo/{idgrupo}', 'ControladorGrupo@guardar');
+    /* --------------------------------------------- */
+    /* CONTROLADOR CARGOS                          */
+    /* --------------------------------------------- */
+    Route::prefix('cargo')->group(function () {
+        Route::get('', 'CargosController@index');
+        Route::get('nuevo', 'CargosController@nuevo');
+        Route::post('nuevo', 'CargosController@guardar');
+        Route::get('cargarGrilla', 'CargosController@cargarGrilla')->name('cargo.cargarGrilla');
+        Route::get('eliminar', 'CargosController@eliminar');
+        Route::get('{id}', 'CargosController@editar');
+        Route::post('{id}', 'CargosController@guardar');
+    });
 
-/* --------------------------------------------- */
-/* CONTROLADOR USUARIO                           */
-/* --------------------------------------------- */
-    Route::get('/admin/usuarios', 'ControladorUsuario@index');
-    Route::get('/admin/usuarios/nuevo', 'ControladorUsuario@nuevo');
-    Route::post('/admin/usuarios/nuevo', 'ControladorUsuario@guardar');
-    Route::post('/admin/usuarios/{usuario}', 'ControladorUsuario@guardar');
-    Route::get('/admin/usuarios/cargarGrilla', 'ControladorUsuario@cargarGrilla')->name('usuarios.cargarGrilla');
-    Route::get('/admin/usuarios/buscarUsuario', 'ControladorUsuario@buscarUsuario');
-    Route::get('/admin/usuarios/{usuario}', 'ControladorUsuario@editar');
+    Route::get('cargos', 'CargosController@index');
 
-/* --------------------------------------------- */
-/* CONTROLADOR MENU                             */
-/* --------------------------------------------- */
-    Route::get('/admin/sistema/menu', 'ControladorMenu@index');
-    Route::get('/admin/sistema/menu/nuevo', 'ControladorMenu@nuevo');
-    Route::post('/admin/sistema/menu/nuevo', 'ControladorMenu@guardar');
-    Route::get('/admin/sistema/menu/cargarGrilla', 'ControladorMenu@cargarGrilla')->name('menu.cargarGrilla');
-    Route::get('/admin/sistema/menu/eliminar', 'ControladorMenu@eliminar');
-    Route::get('/admin/sistema/menu/{id}', 'ControladorMenu@editar');
-    Route::post('/admin/sistema/menu/{id}', 'ControladorMenu@guardar');
+    /* --------------------------------------------- */
+    /* CONTROLADOR CATEGORIAS LABORALES              */
+    /* --------------------------------------------- */
+    Route::prefix('categoria-laboral')->group(function () {
+        Route::get('', 'CategoriasLaboralesController@index');
+        Route::get('nuevo', 'CategoriasLaboralesController@nuevo');
+        Route::post('nuevo', 'CategoriasLaboralesController@guardar');
+        Route::get('cargarGrilla', 'CategoriasLaboralesController@cargarGrilla')->name('categoria.cargarGrilla');
+        Route::get('eliminar', 'CategoriasLaboralesController@eliminar');
+        Route::get('{id}', 'CategoriasLaboralesController@editar');
+        Route::post('{id}', 'CategoriasLaboralesController@guardar');
+    });
+
+    Route::get('categorias-laborales', 'CategoriasLaboralesController@index');
 
 });
 
-/* --------------------------------------------- */
-/* CONTROLADOR PATENTES                          */
-/* --------------------------------------------- */
-Route::get('/admin/patentes', 'ControladorPatente@index');
-Route::get('/admin/patente/nuevo', 'ControladorPatente@nuevo');
-Route::post('/admin/patente/nuevo', 'ControladorPatente@guardar');
-Route::get('/admin/patente/cargarGrilla', 'ControladorPatente@cargarGrilla')->name('patente.cargarGrilla');
-Route::get('/admin/patente/eliminar', 'ControladorPatente@eliminar');
-Route::get('/admin/patente/nuevo/{id}', 'ControladorPatente@editar');
-Route::post('/admin/patente/nuevo/{id}', 'ControladorPatente@guardar');
-
-
-/* --------------------------------------------- */
-/* CONTROLADOR Categorias Cargos                  */
-/* --------------------------------------------- */
-
-Route::get('/admin/cargos', 'CargosController@index');
-Route::get('/admin/cargo/nuevo', 'CargosController@nuevo');
-Route::post('/admin/cargo/nuevo', 'CargosController@guardar');
-Route::get('/admin/cargo/cargarGrilla', 'CargosController@cargarGrilla')->name('cargo.cargarGrilla');
-Route::get('/admin/cargo/eliminar', 'CargosController@eliminar');
-Route::get('/admin/cargo/{id}', 'CargosController@editar');
-Route::post('/admin/cargo/{id}', 'CargosController@guardar');
-
-/* --------------------------------------------- */
-/* CONTROLADOR Categorias laborales                         */
-/* --------------------------------------------- */
-Route::get('/admin/categorias-laborales', 'CategoriasLaboralesController@index');
-Route::get('/admin/categoria-laboral/nuevo', 'CategoriasLaboralesController@nuevo');
-Route::post('/admin/categoria-laboral/nuevo', 'CategoriasLaboralesController@guardar');
-Route::get('/admin/categoria-laboral/cargarGrilla', 'CategoriasLaboralesController@cargarGrilla')->name('categoria.cargarGrilla');
-Route::get('/admin/categoria-laboral/eliminar', 'CategoriasLaboralesController@eliminar');
-Route::get('/admin/categoria-laboral/{id}', 'CategoriasLaboralesController@editar');
-Route::post('/admin/categoria-laboral/{id}', 'CategoriasLaboralesController@guardar');
